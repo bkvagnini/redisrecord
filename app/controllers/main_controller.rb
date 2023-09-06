29b2847faded49fd230029d85b9c0874a43c817b
Brian K. Vagnini <brian@vagnini.net>
2023-09-06 14:57:46 -0400

    Change link structure to match GoRails tutorial.

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,17 @@
+class MainController < ApplicationController
+#class StaticController < ApplicationController
+#  def about
+#  end
+
+  def index
+  end
+
+#  def merch
+#  end
+
+#  def show
+#  end
+
+#  def contact
+#  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'static/about'
-  get 'static/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "static#home" 
+  #get 'static/about'
+  #get 'static/home'
+  #root "static#home"
+
+  get "about", to: "about#index"
+  root to: "main#index"
 end
